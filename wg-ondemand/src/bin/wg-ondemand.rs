a83a4382956b1604367ca4dd286899ebb4c91652
Hayden Stith <haydenstith@protonmail.com>
2022-09-28 09:41:07 -0400

    Parse command line arguments

--- wg-ondemand/src/bin/wg-ondemand.rs
+++ wg-ondemand/src/bin/wg-ondemand.rs
@@ -6,8 +6,8 @@ use wg_ondemand::cmd::parse_arguments;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let _parsed_args: Vec<[String; 2]> = parse_arguments(&args);
+    let parsed_args: Vec<[String; 2]> = parse_arguments(&args);
 
+    println!("{:?}", parsed_args);
     println!("{}", get_ssid());
-    println!("Hello, world!");
 }
--- wg-ondemand/src/cmd/mod.rs
+++ wg-ondemand/src/cmd/mod.rs
@@ -1,10 +1,34 @@
 pub fn parse_arguments(args: &Vec<String>) -> Vec<[String; 2]> {
     let mut parsed_args: Vec<[String; 2]> = Vec::new();
-    
-    parsed_args.insert(0, [String::from(""), String::from("")]);
+    let mut append_at_end: Vec<String> = Vec::new();
 
+    let mut prev_flag: bool = false;
     for arg in args {
-        println!("{}", arg);
+        if arg.get(..1).unwrap() == "-" && arg.get(..2).unwrap() != "--" {
+            prev_flag = true;
+            for i in 1..arg.len() {
+                let char = arg.get(i..i+1).unwrap();
+                parsed_args.push([String::from(char), String::from("")]);
+            }
+        } else if arg.get(..2).unwrap() == "--" {
+            prev_flag = true;
+            parsed_args.push([String::from(arg.get(2..).unwrap()), String::from("")]);
+        } else {
+            if prev_flag == true {
+                let flag: &str = parsed_args[parsed_args.len() - 1][0].as_str();
+                let new_arg: [String; 2] = [String::from(flag), arg.clone()];
+                parsed_args.insert(parsed_args.len() - 1, new_arg);
+                parsed_args.remove(parsed_args.len() - 1);
+            } else {
+                append_at_end.push(String::from(arg));
+            }
+            prev_flag = false;
+        }
+    }
+
+    for a in append_at_end {
+        let arg: [String; 2] = [String::from(""), a];
+        parsed_args.push(arg);
     }
 
     parsed_args
